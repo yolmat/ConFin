6a7be78fd7e2200ae2706dc0b5244264ad934249
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-09-30 03:33:13 -0300

    Init project EcoFin

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import * as process from "process";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
