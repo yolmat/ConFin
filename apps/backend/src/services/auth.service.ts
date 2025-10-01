528f243468ea365e6a35281f743d756b3c6702ee
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-10-01 03:16:13 -0300

    Create Auth service login

---
+++ apps/backend/src/services/auth.service.ts
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { prisma } from "../db/client";
+
+const JWT_SECRET = process.env.JWT_SECRET || "super-secret-key";
+
+export async function login(email: string, password: string) {
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) throw new Error("Usuário não encontrado");
+
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (!isValid) throw new Error("Senha inválida");
+
+  const token = jwt.sign(
+    { userId: user.id, email: user.email, role: user.role },
+    JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  return { token, user };
+}
