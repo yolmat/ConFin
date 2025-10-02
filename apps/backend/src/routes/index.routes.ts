0611321cc002ceab5102fa73cfcd859cfbe8da3f
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-10-02 02:58:14 -0300

    Route user ajust

--- apps/backend/src/routes/index.routes.ts
+++ apps/backend/src/routes/index.routes.ts
@@ -1,10 +1,12 @@
 import { Application } from "express";
 import authRoutes from "./auth.routes";
 import transactionsRoutes from "./transactions.routes";
+import userRoutes from "./user.routes";
 
 const routes = (app: Application) => {
   authRoutes(app);
   transactionsRoutes(app);
+  userRoutes(app);
 };
 
 export default routes;
