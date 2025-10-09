fe6d0af4d2b238f20b27c14c4bef656166c4b057
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-10-09 03:30:52 -0300

    user handler when creating transaction

--- apps/backend/src/controllers/transaction.controller.ts
+++ apps/backend/src/controllers/transaction.controller.ts
@@ -4,8 +4,15 @@
 import { Request, Response } from "express";
 import { prisma } from "../db/client";
 import * as transactionService from "../services/transaction.service";
+import jwt from "jsonwebtoken";
 
 export const createTransaction = async (req: Request, res: Response) => {
+  const authHeader = req.headers.authorization as any;
+  const [, token] = authHeader.split(" ");
+  const decoded = jwt.decode(token) as any;
+
+  if (req.body.id !== decoded.userId)
+    return res.status(400).json({ message: "Usuarios divergentes" });
   try {
     const result = await transactionService.createTransaction(
       req.body.id,
