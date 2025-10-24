237f35d1bad7bea24acebb7ccac0efa08c60c181
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-10-24 00:30:19 -0300

    Add validation transatiuons

--- apps/backend/src/controllers/transaction.controller.ts
+++ apps/backend/src/controllers/transaction.controller.ts
@@ -5,8 +5,14 @@ import { Request, Response } from "express";
 import { prisma } from "../db/client";
 import * as transactionService from "../services/transaction.service";
 import jwt from "jsonwebtoken";
+import { transactionCreateSchema } from "../schemas/transaction";
 
 export const createTransaction = async (req: Request, res: Response) => {
+  const result = transactionCreateSchema.safeParse(req.body);
+
+  if (!result.success)
+    return res.status(400).json({ error: result.error.format() });
+
   const authHeader = req.headers.authorization as any;
   const [, token] = authHeader.split(" ");
   const decoded = jwt.decode(token) as any;
@@ -23,6 +29,7 @@ export const createTransaction = async (req: Request, res: Response) => {
     res.status(400).json(error);
   }
 };
+
 export const allTranisitonsGlobal = async (req: Request, res: Response) => {
   try {
     const transitions = await prisma.transaction.findMany();
